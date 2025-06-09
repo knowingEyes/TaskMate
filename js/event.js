91bcc2c8ae95f366e738a63812ae4f1ddc373f62
oluwa Ibrahim <mj9762651@gmail.com>
2025-06-09 13:58:59 +0100

    Sucessfully implemented theme toggling for inner and landing page

--- js/event.js
+++ js/event.js
@@ -1,7 +1,14 @@
-import { TransitionToInnerPage } from "./helper.js";
+import { TransitionToInnerPage,ToggleTheme } from "./helper.js";
 
 const getStartedBtn = document.querySelector("#getStartedBtn");
+const ToggleBtn = document.querySelector(".toggle")
+
 getStartedBtn.addEventListener("click", (e) => {
   TransitionToInnerPage(e);
 });
+
+ToggleBtn.addEventListener("click",()=> {
+  let themeIcon = document.querySelectorAll("svg")
+    ToggleTheme(themeIcon)
+})
 
--- js/helper.js
+++ js/helper.js
@@ -1,8 +1,23 @@
-
 export function TransitionToInnerPage(e) {
   let landing = e.target.closest("#landingPage");
   landing.classList.add("fadeOut");
   landing.addEventListener("animationend", () => {
     landing.remove();
+  });
+}
+
+export function ToggleTheme(elements) {
+  document.querySelector("#landingPage").classList.toggle("dark");
+  document.querySelector("#innerPage").classList.toggle("dark");
+  ShowHide(elements);
+}
+
+export function ShowHide(elements) {
+  Array.from(elements).forEach((element) => {
+    if (!element.classList.contains("hidden")) {
+      element.classList.add("hidden");
+    } else {
+      element.classList.remove("hidden");
+    }
   });
 }
