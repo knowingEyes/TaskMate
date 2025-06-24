3a0cafc6851df1ddb71f60d01b90477b70dfa299
oluwa Ibrahim <mj9762651@gmail.com>
2025-06-24 21:01:07 +0100

    feat: complete landing-page animations using scrollreveal

--- js/ui_Effects.js
+++ js/ui_Effects.js
@@ -12,3 +12,24 @@ export function dragAndDrop() {
 }
 dragAndDrop();
 
+function animate(el, obj = {}) {
+    ScrollReveal().reveal(el,  { 
+    origin : "top",
+    distance: "20px",
+    duration :"500",
+    opacity: 0,
+    easing : "ease-out",
+    reset: true,
+    mobile: true,
+    cleanup : true,
+    ...obj,
+});
+}
+animate(".logoCon", {delay : 100,reset : false})
+animate("#title", {delay : 0})
+animate("#subtitle", {delay : 100})
+animate("#getStartedBtn", {origin : "bottom",delay : 200})
+animate(".left", {delay : 0, distance : "60px", origin : "left"})
+animate(".right", {delay : 100, distance : "60px", origin : "right"})
+animate(".leftLast", {delay : 200, distance : "60px", origin : "left"})
+
